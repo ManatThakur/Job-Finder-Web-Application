c672648a7aca31024662c64301415b8dbffc011e
ManatThakur <manatthakur7@gmail.com>
2026-09-03 21:16:19 +0530

    Fix Vercel serverless handler

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ const app=require('./src/app');
 const connectDB = require('./src/db/db');
 const PORT = process.env.PORT || 3005;
 
-connectDB()
-.then(()=>{
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} http://localhost:${PORT}`);
-});
-})
-.catch((err)=>{
-    console.log(err);
-});
+if (require.main === module) {
+    connectDB()
+    .then(()=>{
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT} http://localhost:${PORT}`);
+        });
+    })
+    .catch((err)=>{
+        console.error(err);
+        process.exitCode = 1;
+    });
+} else {
+    module.exports = async (req, res) => {
+        await connectDB();
+        return app(req, res);
+    };
+}
